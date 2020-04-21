037beeaf5f089984b23647f2bf1e81ebf790d27b
Daniel Robinson <Daniel@daniels-air.home>
2020-04-21 18:53:06 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     msg1.textContent = 'Getting weather for ' + location + '...'
     msg2.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return msg1.textContent = data.error
